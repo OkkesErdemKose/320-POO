76f393055a75f3d18d1be16954b28f2a5a66f260
Ökkes Erdem Köse <pl23ksc@eduvaud.ch>
2025-08-29 11:12:52 +0200

    Ajout des dossiers snail_IP (Impératif Procédural) et snail_OO Orienté Objets)

---
+++ personnel/snail_IP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace escargot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SNAIL = "_@_ö";
+            const string DEAD_SNAIL = "____";
+            const string NO_SNAIL = "    ";
+
+            Console.CursorVisible = false;
+
+
+            for (int i = 0; i < 20; i++)
+            {
+
+                Console.CursorLeft = i + 4;
+                Console.Write(SNAIL);
+
+                Console.CursorLeft = i;
+                Console.Write(NO_SNAIL);
+
+
+                Thread.Sleep(100);
+            }
+
+            Console.Write(DEAD_SNAIL);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
